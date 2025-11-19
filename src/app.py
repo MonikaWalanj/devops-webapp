e8f5461327dc9a50eb48eab4dbca37645d65f320
Ubuntu <ubuntu@ip-172-31-21-61.ec2.internal>
2025-11-19 13:15:20 +0000

    code change

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
 
-    return "Hello from Devops Project! I am Monika testing ci code"
+    return "Hello from Devops Project! I am Monika testing ci code with new process"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
