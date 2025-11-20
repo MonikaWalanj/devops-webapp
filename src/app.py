8e5c76b9b963f90354f521acda8454b7658016cc
MonikaWalanj <88365421+MonikaWalanj@users.noreply.github.com>
2025-11-20 14:58:25 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home()
+def home():
 
     return "Hello from Devops Project! I am Monika testing ci and cd code on github with docker"
 
